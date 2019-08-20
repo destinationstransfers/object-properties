983146ef2efca580f797386ac321cfa04398a3db
Tino Vyatkin <tino@vtkn.io>
2019-08-19 22:41:53 -0400

    fix overload

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,17 @@
-
-
-export function hasDeepProperty(obj: object, propertyPath: string | string[]): boolean;
-export function getDeepProperty(obj: object, propertyPath: string | string[]): any;
-export function setDeepProperty(obj?: object, property: string | string[] | Object, value?: any): object;
+export function hasDeepProperty(
+  obj: object,
+  propertyPath: string | string[]
+): boolean;
+export function getDeepProperty(
+  obj: object,
+  propertyPath: string | string[]
+): any;
+export function setDeepProperty(
+  obj?: object,
+  property: string | string[],
+  value: any
+): object;
+export function setDeepProperty(
+  obj?: object,
+  properties: Record<string, any>
+): object;
